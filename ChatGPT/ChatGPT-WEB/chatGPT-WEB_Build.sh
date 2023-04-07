c1399e2260dc539699967e8719007febf6cb1f87
dqzboy <42825450+dqzboy@users.noreply.github.com>
2023-04-07 11:58:37 +0800

    Update chatGPT-WEB_Build.sh

--- ChatGPT/ChatGPT-WEB/chatGPT-WEB_Build.sh
+++ ChatGPT/ChatGPT-WEB/chatGPT-WEB_Build.sh
@@ -34,6 +34,8 @@ read -e -p "请选择你的服务器网络环境[国外1/国内2]： " NETWORK
 if [ ${NETWORK} == 1 ];then
     if [ -f /etc/redhat-release ]; then
         echo "This is CentOS."
+        yum install nscd -y 2>/dev/null
+        systemctl restart nscd.service
         curl -sO -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/dqzboy/ShellProject/main/ChatGPT/ChatGPT-WEB/chatGPT-WEB_C.sh
         source ${PWD}/chatGPT-WEB_C.sh
     elif [ -f /etc/lsb-release ]; then
@@ -53,6 +55,8 @@ if [ ${NETWORK} == 1 ];then
 elif [ ${NETWORK} == 2 ];then
         if [ -f /etc/redhat-release ]; then
         echo "This is CentOS."
+        yum install nscd -y 2>/dev/null
+        systemctl restart nscd.service
         curl -sO -H 'Cache-Control: no-cache' https://ghproxy.com/https://raw.githubusercontent.com/dqzboy/ShellProject/main/ChatGPT/ChatGPT-WEB/chatGPT-WEB_C.sh
         source ${PWD}/chatGPT-WEB_C.sh
     elif [ -f /etc/lsb-release ]; then
