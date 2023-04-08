3385f5399716b874f0b43cc0654d0da6a56a9cd2
dqzboy <42825450+dqzboy@users.noreply.github.com>
2023-04-08 20:53:57 +0800

    Update chatGPT-WEB_U.sh

--- ChatGPT/ChatGPT-WEB/chatGPT-WEB_U.sh
+++ ChatGPT/ChatGPT-WEB/chatGPT-WEB_U.sh
@@ -260,9 +260,11 @@ sleep 10
 if systemctl status chatgpt-web | grep -q "active (running)"; then
   echo "chatgpt-web后端服务已成功启动"
 else
+  echo
   ${SETCOLOR_RED} && echo "----------------chatgpt-web后端服务启动失败，请查看错误日志 ↓↓↓----------------" && ${SETCOLOR_NORMAL}
   journalctl -u chatgpt-web --no-pager
   ${SETCOLOR_RED} && echo "----------------chatgpt-web后端服务启动失败，请查看错误日志 ↑↑↑----------------" && ${SETCOLOR_NORMAL}
+  echo
 fi
 
 # 拷贝前端刷新Nginx服务
