412668669e9f4065b7b549fac07f4590122cb4c9
dqzboy <42825450+dqzboy@users.noreply.github.com>
2023-04-15 21:28:38 +0800

    Update chatGPT-WEB_U.sh

--- ChatGPT/ChatGPT-WEB/chatGPT-WEB_U.sh
+++ ChatGPT/ChatGPT-WEB/chatGPT-WEB_U.sh
@@ -138,9 +138,9 @@ ${SETCOLOR_NORMAL}
 # 交互输入Nginx根目录(提前进行创建好)
 if [ -f .input ]; then
   last_input=$(cat .input)
-  read -e -p "WEB存储绝对路径(回车默认Nginx路径)[上次执行：${last_input}]：" WEBDIR
+  read -e -p "WEB存储绝对路径[上次记录：${last_input} 回车用上次记录]：" WEBDIR
   if [ -z "${WEBDIR}" ];then
-      WEBDIR="/usr/share/nginx/html"
+      WEBDIR="$last_input"
       ${SETCOLOR_SKYBLUE} && echo "chatGPT-WEB存储路径：${WEBDIR}" && ${SETCOLOR_NORMAL}
   else
       ${SETCOLOR_SUCCESS} && echo "chatGPT-WEB存储路径：${WEBDIR}" && ${SETCOLOR_NORMAL}
@@ -154,26 +154,57 @@ else
       ${SETCOLOR_SUCCESS} && echo "chatGPT-WEB存储路径：${WEBDIR}" && ${SETCOLOR_NORMAL}
   fi
 fi
-
 echo "${WEBDIR}" > .input
 
 echo "------------------------------------------------------------------------------------------------"
-read -e -p "修改用户默认名称/描述/头像信息,请用空格分隔[留空则保持默认!]：" USERINFO
-if [ -z "${USERINFO}" ];then
-    ${SETCOLOR_SKYBLUE} && echo "没有输入,保持默认" && ${SETCOLOR_NORMAL}
+if [ -f .userinfo ]; then
+  user_input=$(cat .userinfo)
+  read -e -p "修改用户默认名称/描述/头像信息,请用空格分隔[上次记录：${user_input} 回车用上次记录]：" USERINFO
+  if [ -z "${USERINFO}" ];then
+      USERINFO="$user_input"
+      USER=$(echo "${USERINFO}" | cut -d' ' -f1)
+      INFO=$(echo "${USERINFO}" | cut -d' ' -f2)
+      AVATAR=$(echo "${USERINFO}" | cut -d' ' -f3)
+      ${SETCOLOR_SUCCESS} && echo "当前用户默认名称为：${USER}" && ${SETCOLOR_NORMAL}
+      ${SETCOLOR_SUCCESS} && echo "当前描述信息默认为：${INFO}" && ${SETCOLOR_NORMAL}
+      # 修改个人信息
+      sed -i "s/ChenZhaoYu/${USER}/g" ${ORIGINAL}/${CHATDIR}/src/store/modules/user/helper.ts
+      sed -i "s#Star on <a href=\"https://github.com/Chanzhaoyu/chatgpt-bot\" class=\"text-blue-500\" target=\"_blank\" >Github</a>#${INFO}#g" ${ORIGINAL}/${CHATDIR}/src/store/modules/user/helper.ts
+      sed -i "s#https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg#${AVATAR}#g" ${ORIGINAL}/${CHATDIR}/src/store/modules/user/helper.ts
+      # 删除配置里面的GitHub相关信息内容(可选，建议保留，尊重项目作者成果)
+      #sed -i '/<div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">/,/<\/div>/d' ${ORIGINAL}/${CHATDIR}/src/components/common/Setting/About.vue
+  else
+      USER=$(echo "${USERINFO}" | cut -d' ' -f1)
+      INFO=$(echo "${USERINFO}" | cut -d' ' -f2)
+      AVATAR=$(echo "${USERINFO}" | cut -d' ' -f3)
+      ${SETCOLOR_SUCCESS} && echo "当前用户默认名称为：${USER}" && ${SETCOLOR_NORMAL}
+      ${SETCOLOR_SUCCESS} && echo "当前描述信息默认为：${INFO}" && ${SETCOLOR_NORMAL}
+      # 修改个人信息
+      sed -i "s/ChenZhaoYu/${USER}/g" ${ORIGINAL}/${CHATDIR}/src/store/modules/user/helper.ts
+      sed -i "s#Star on <a href=\"https://github.com/Chanzhaoyu/chatgpt-bot\" class=\"text-blue-500\" target=\"_blank\" >Github</a>#${INFO}#g" ${ORIGINAL}/${CHATDIR}/src/store/modules/user/helper.ts
+      sed -i "s#https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg#${AVATAR}#g" ${ORIGINAL}/${CHATDIR}/src/store/modules/user/helper.ts
+      # 删除配置里面的GitHub相关信息内容(可选，建议保留，尊重项目作者成果)
+      #sed -i '/<div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">/,/<\/div>/d' ${ORIGINAL}/${CHATDIR}/src/components/common/Setting/About.vue
+   fi
 else
-    USER=$(echo "${USERINFO}" | cut -d' ' -f1)
-    INFO=$(echo "${USERINFO}" | cut -d' ' -f2)
-    AVATAR=$(echo "${USERINFO}" | cut -d' ' -f3)
-    ${SETCOLOR_SUCCESS} && echo "当前用户默认名称为：${USER}" && ${SETCOLOR_NORMAL}
-    ${SETCOLOR_SUCCESS} && echo "当前描述信息默认为：${INFO}" && ${SETCOLOR_NORMAL}
-    # 修改个人信息
-    sed -i "s/ChenZhaoYu/${USER}/g" ${ORIGINAL}/${CHATDIR}/src/store/modules/user/helper.ts
-    sed -i "s#Star on <a href=\"https://github.com/Chanzhaoyu/chatgpt-bot\" class=\"text-blue-500\" target=\"_blank\" >Github</a>#${INFO}#g" ${ORIGINAL}/${CHATDIR}/src/store/modules/user/helper.ts
-    sed -i "s#https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg#${AVATAR}#g" ${ORIGINAL}/${CHATDIR}/src/store/modules/user/helper.ts
-    # 删除配置里面的GitHub相关信息内容(可选，建议保留，尊重项目作者成果)
-    #sed -i '/<div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">/,/<\/div>/d' ${ORIGINAL}/${CHATDIR}/src/components/common/Setting/About.vue
+   read -e -p "修改用户默认名称/描述/头像信息,请用空格分隔[回车保持默认不做修改]：" USERINFO
+   if [ -z "${USERINFO}" ];then
+      ${SETCOLOR_SKYBLUE} && echo "没有输入,保持默认" && ${SETCOLOR_NORMAL}
+   else
+      USER=$(echo "${USERINFO}" | cut -d' ' -f1)
+      INFO=$(echo "${USERINFO}" | cut -d' ' -f2)
+      AVATAR=$(echo "${USERINFO}" | cut -d' ' -f3)
+      ${SETCOLOR_SUCCESS} && echo "当前用户默认名称为：${USER}" && ${SETCOLOR_NORMAL}
+      ${SETCOLOR_SUCCESS} && echo "当前描述信息默认为：${INFO}" && ${SETCOLOR_NORMAL}
+      # 修改个人信息
+      sed -i "s/ChenZhaoYu/${USER}/g" ${ORIGINAL}/${CHATDIR}/src/store/modules/user/helper.ts
+      sed -i "s#Star on <a href=\"https://github.com/Chanzhaoyu/chatgpt-bot\" class=\"text-blue-500\" target=\"_blank\" >Github</a>#${INFO}#g" ${ORIGINAL}/${CHATDIR}/src/store/modules/user/helper.ts
+      sed -i "s#https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg#${AVATAR}#g" ${ORIGINAL}/${CHATDIR}/src/store/modules/user/helper.ts
+      # 删除配置里面的GitHub相关信息内容(可选，建议保留，尊重项目作者成果)
+      #sed -i '/<div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">/,/<\/div>/d' ${ORIGINAL}/${CHATDIR}/src/components/common/Setting/About.vue
+   fi
 fi
+[ -n "${USERINFO}" ] && echo "${USERINFO}" > .userinfo
 }
 
 
