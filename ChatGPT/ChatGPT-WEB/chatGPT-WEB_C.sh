55486ef62deddb38bea6f36f93811cf711afc273
dqzboy <42825450+dqzboy@users.noreply.github.com>
2023-04-16 14:36:45 +0800

    Update chatGPT-WEB_C.sh

--- ChatGPT/ChatGPT-WEB/chatGPT-WEB_C.sh
+++ ChatGPT/ChatGPT-WEB/chatGPT-WEB_C.sh
@@ -307,8 +307,6 @@ ExecStart=$(which pnpm) run start
 Restart=always
 ExecReload=/bin/kill -s HUP \$MAINPID
 ExecStop=/bin/kill -s QUIT \$MAINPID
-StandardOutput=null
-StandardError=null
 Restart=always
 TimeoutStopSec=5s
 
