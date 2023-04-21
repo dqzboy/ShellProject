53fedc43e6f1b80070d5b81ae99a7d64cc90300d
dqzboy <42825450+dqzboy@users.noreply.github.com>
2023-04-21 12:36:12 +0800

    Update chatGPT-WEB_C.sh

--- ChatGPT/ChatGPT-WEB/chatGPT-WEB_C.sh
+++ ChatGPT/ChatGPT-WEB/chatGPT-WEB_C.sh
@@ -83,7 +83,7 @@ else
   yum -y install wget git openssl-devel pcre-devel zlib-devel gd-devel &>/dev/null
   yum -y install pcre2 &>/dev/null
   wget http://nginx.org/packages/centos/7/x86_64/RPMS/nginx-1.22.1-1.el7.ngx.x86_64.rpm &>/dev/null
-  yum -y install nginx-1.22.1-1.el7.ngx.x86_64.rpm 2>&1 >/dev/null | grep -E "error|fail|warning"
+  yum -y install nginx-1.22.1-1.el7.ngx.x86_64.rpm &>/dev/null
   if [ $? -ne 0 ]; then
     echo "安装失败，请手动安装，安装成功之后再次执行脚本！"
     echo "命令：wget http://nginx.org/packages/centos/7/x86_64/RPMS/nginx-1.22.1-1.el7.ngx.x86_64.rpm && yum -y install nginx-1.22.1-1.el7.ngx.x86_64.rpm"
