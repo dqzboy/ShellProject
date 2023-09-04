b2fe706ff0682a287025da1c5c9725bd8e4d73d2
dqzboy <42825450+dqzboy@users.noreply.github.com>
2023-09-04 09:59:20 +0800

    Create unlawful.sh

---
+++ IPTABLES/unlawful.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+#===============================================================================
+#
+#          FILE: unlawful.sh
+# 
+#         USAGE: ./unlawful.sh
+#   DESCRIPTION: 通过 iptables 配置防火墙规则，限制非法连接，保护系统安全
+# 
+#  ORGANIZATION: dqzboy.com
+#===============================================================================
+
+# 允许所有本地回环连接
+iptables -A INPUT -i lo -j ACCEPT
+
+# 允许已经建立的、相关的连接通过
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# 允许SSH连接
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+# 允许HTTP和HTTPS连接
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+# 允许Ping连接
+iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+
+# 阻止所有其他连接
+iptables -A INPUT -j DROP
+
+# 保存防火墙规则
+iptables-save > /etc/sysconfig/iptables
+
+# 设置防火墙开机启动
+systemctl enable iptables.service
+
+# 启动防火墙
+systemctl start iptables.service
+
+# 显示防火墙状态
+systemctl status iptables.service
