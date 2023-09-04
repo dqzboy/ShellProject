1382686b4d3e713163581570f0ab82883c3a4cd4
dqzboy <42825450+dqzboy@users.noreply.github.com>
2023-09-04 09:58:31 +0800

    Create firewalld_Closure.sh

---
+++ IPTABLES/firewalld_Closure.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#===============================================================================
+#
+#          FILE: firewalld_Closure.sh
+# 
+#         USAGE: ./firewalld_Closure.sh prot
+#   DESCRIPTION: 通过firewalld管理服务端口
+# 
+#  ORGANIZATION: dqzboy.com
+#===============================================================================
+PORT="$1"
+White_list=`firewall-cmd --query-port=${PORT}/tcp`
+fireState=`firewall-cmd --state`
+if [[ $fireState == "running" ]];then
+    echo "防火墙开启>>>>>>>>"
+    if [ "$PORT" != '' ] && [ "$PORT" -le "65535" ];then
+        if [[ $White_list == yes ]];then
+            echo "端口已在放通名单中,接下来关闭端口"
+            firewall-cmd --permanent --zone=public --remove-port=${PORT}/tcp
+            firewall-cmd --reload
+        else
+            echo "名单中无此端口号,无需进行关闭"
+        fi
+    else
+        echo "参数无效，本次流程结束"
+        exit 1
+    fi
+else
+    echo "防火墙关闭>>>>>>>>"
+    exit 2
+fi
