4f17fd258ed06bbde40cc499fe31c8ffb8806c5b
dqzboy <42825450+dqzboy@users.noreply.github.com>
2024-03-03 18:25:09 +0800

    Create manage_cow.sh

---
+++ ChatGPT/chatgpt-on-wechat/manage_cow.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+# 定义颜色
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# 指定app.py文件所在的目录
+APP_DIR="/data/chatgpt-on-wechat/chatgpt-on-wechat-fork"
+APP_FILE="app.py"
+
+# 获取Python应用程序的进程ID (PID)
+get_pid() {
+    echo $(ps aux | grep "[p]ython3 $APP_FILE" | awk '{print $2}')
+}
+
+# 函数：启动Python应用程序
+start_app() {
+    pid=$(get_pid)
+    if [ ! -z "$pid" ]; then
+        echo -e "${GREEN}应用程序已经在运行。 PID: $pid${NC}"
+    else
+        cd $APP_DIR
+        if [ $? -eq 0 ]; then
+            nohup python3 $APP_FILE > output.log 2>&1 &
+            echo -e "${GREEN}应用程序已启动。${NC}"
+        else
+            echo -e "${RED}无法切换到应用程序目录: $APP_DIR ${NC}"
+        fi
+    fi
+}
+
+# 函数：停止Python应用程序
+stop_app() {
+    pid=$(get_pid)
+    if [ ! -z "$pid" ]; then
+        kill -9 $pid
+        echo -e "${GREEN}应用程序已停止。${NC}"
+    else
+        echo -e "${RED}应用程序未在运行。${NC}"
+    fi
+}
+
+# 函数：查看Python应用程序状态
+check_status() {
+    pid=$(get_pid)
+    if [ ! -z "$pid" ]; then
+        echo -e "${GREEN}应用程序正在运行。 PID: $pid${NC}"
+    else
+        echo -e "${RED}应用程序未在运行。${NC}"
+    fi
+}
+
+# 显示表格样式的菜单
+function show_menu {
+    echo -e "${GREEN}Python 应用管理菜单${NC}"
+    echo "+-------------------+-----------------------+"
+    echo "| 选项 | 描述                               |"
+    echo "+-------------------+-----------------------+"
+    echo "| 1    | 启动 Python 应用程序              |"
+    echo "| 2    | 停止 Python 应用程序              |"
+    echo "| 3    | 查看 Python 应用程序的运行状态    |"
+    echo "| 4    | 退出                               |"
+    echo "+-------------------+-----------------------+"
+}
+
+# 主菜单循环
+while true; do
+    show_menu
+    read -e -p "$(echo -e ${GREEN}"请选择操作（1-4）: "${NC})" choice
+    case "$choice" in
+        1) start_app ;;
+        2) stop_app ;;
+        3) check_status ;;
+        4) exit ;;
+        *) echo -e "${RED}无效选择，请输入1-4之间的数字。${NC}" ;;
+    esac
+done
