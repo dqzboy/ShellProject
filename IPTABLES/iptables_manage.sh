26452c95912f38e25951d4203c528809018a2838
dqzboy <42825450+dqzboy@users.noreply.github.com>
2024-03-05 10:29:19 +0800

    Update iptables_manage.sh

--- IPTABLES/iptables_manage.sh
+++ IPTABLES/iptables_manage.sh
@@ -16,36 +16,38 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+# 保存 iptables 规则的函数（Debian/Ubuntu）
+function save_rules {
+    sudo iptables-save | sudo tee /etc/sysconfig/iptables
+    echo -e "${GREEN}规则已保存。${NC}"
+}
+
 # 函数：禁止IP访问
 function block_ip {
-    read -e -p "$(echo -e ${GREEN}"输入要屏蔽的IP地址: "${NC})" ip
+    read -e -p "$(echo -e ${GREEN}"输入要屏蔽的IP地址(多个IP用空格分隔): "${NC})" ip
     for i in $ip; do
         if sudo iptables -L INPUT -v -n | grep -q "$i"; then
             echo -e "${RED}IP $i 已被屏蔽。${NC}"
         else
             sudo iptables -I INPUT -s "$i" -j DROP
             echo -e "${GREEN}IP $i 已被屏蔽。${NC}"
+            save_rules
         fi
     done
 }
 
 # 函数：禁止端口访问
 function block_port {
-    read -e -p "$(echo -e ${GREEN}"输入要屏蔽的端口号: "${NC})" port
+    read -e -p "$(echo -e ${GREEN}"输入要屏蔽的端口号(多个端口用空格分隔): "${NC})" port
     for p in $port; do
         if sudo iptables -L INPUT -v -n | grep -q "dpt:$p .* DROP"; then
             echo -e "${RED}端口 $p 已被屏蔽。${NC}"
         else
             sudo iptables -A INPUT -p tcp --dport "$p" -j DROP
             echo -e "${GREEN}端口 $p 已被屏蔽。${NC}"
+            save_rules
         fi
     done
-}
-
-# 函数：准备放通IP访问
-function prepare_to_allow_ip {
-    list_blocked_ips
-    allow_ip
 }
 
 # 函数：放通IP访问
@@ -55,16 +57,11 @@ function allow_ip {
         if sudo iptables -L INPUT -v -n | grep -q "$ip"; then
             sudo iptables -D INPUT -s "$ip" -j DROP
             echo -e "${GREEN}IP $ip 已被放通。${NC}"
+            save_rules
         else
             echo -e "${RED}IP $ip 没有被屏蔽，无需放通。${NC}"
         fi
     done
-}
-
-# 函数：准备放通端口访问
-function prepare_to_allow_port {
-    list_blocked_ports
-    allow_port
 }
 
 # 函数：放通端口访问
@@ -74,6 +71,7 @@ function allow_port {
         if sudo iptables -L INPUT -v -n | grep -q "dpt:$port .* DROP"; then
             sudo iptables -D INPUT -p tcp --dport "$port" -j DROP
             echo -e "${GREEN}端口 $port 已被放通。${NC}"
+            save_rules
         else
             echo -e "${RED}端口 $port 没有被屏蔽，无需放通。${NC}"
         fi
@@ -97,8 +95,8 @@ function show_menu {
     echo -e "${GREEN}IP和端口管理菜单${NC}"
     echo "1) 禁止IP访问"
     echo "2) 禁止端口访问"
-    echo "3) 准备放通IP访问"
-    echo "4) 准备放通端口访问"
+    echo "3) 放通IP访问"
+    echo "4) 放通端口访问"
     echo "5) 列出所有被禁止的IP"
     echo "6) 列出所有被禁止的端口"
     echo "7) 退出"
@@ -108,7 +106,7 @@ function show_menu {
 while true; do
     show_menu
     read -e -p "$(echo -e ${GREEN}"请选择操作（1-7）: "${NC})" choice
-    
+
     case "$choice" in
         1)
             block_ip
@@ -117,10 +115,10 @@ while true; do
             block_port
             ;;
         3)
-            prepare_to_allow_ip
+            allow_ip
             ;;
         4)
-            prepare_to_allow_port
+            allow_port
             ;;
         5)
             list_blocked_ips
