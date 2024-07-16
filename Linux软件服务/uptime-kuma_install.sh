231a2fc96a8ede616d50df4854184f787e9e942d
dqzboy <42825450+dqzboy@users.noreply.github.com>
2024-07-16 23:54:47 +0800

    Update uptime-kuma_install.sh

--- Linux软件服务/uptime-kuma_install.sh
+++ Linux软件服务/uptime-kuma_install.sh
@@ -27,6 +27,21 @@ GREEN="\033[0;32m"
 RED="\033[31m"
 YELLOW="\033[33m"
 RESET="\033[0m"
+BLUE="\033[0;34m"
+MAGENTA="\033[0;35m"
+CYAN="\033[0;36m"
+WHITE="\033[1;37m"
+BLACK="\033[0;30m"
+LIGHT_GREEN="\033[1;32m"
+LIGHT_RED="\033[1;31m"
+LIGHT_YELLOW="\033[1;33m"
+LIGHT_BLUE="\033[1;34m"
+LIGHT_MAGENTA="\033[1;35m"
+LIGHT_CYAN="\033[1;36m"
+BOLD="\033[1m"
+UNDERLINE="\033[4m"
+BLINK="\033[5m"
+REVERSE="\033[7m"
 
 INFO="[${GREEN}INFO${RESET}]"
 ERROR="[${RED}ERROR${RESET}]"
@@ -41,18 +56,25 @@ function WARN() {
     echo -e "${WARN} ${1}"
 }
 
+function SEPARATOR() {
+    echo -e "${INFO}${BOLD}${LIGHT_BLUE}======================== ${1} ========================${RESET}"
+}
+
+save_path="/data/uptime"
+mkdir -p $save_path && cd /data/uptime
+url="https://raw.githubusercontent.com/louislam/uptime-kuma/master/compose.yaml"
 
 function CHECK_OS() {
-INFO "======================= 检查环境 ======================="
+SEPARATOR "检查环境"
+OSVER=$(cat /etc/os-release | grep -o '[0-9]' | head -n 1)
+
 if [ -f /etc/os-release ]; then
     . /etc/os-release
 else
-    ERROR "无法确定发行版"
+    echo "无法确定发行版"
     exit 1
 fi
 
-
-# 根据发行版选择存储库类型
 case "$ID" in
     "centos")
         repo_type="centos"
@@ -73,27 +95,167 @@ case "$ID" in
         repo_type="centos"
         ;;
     *)
-        WARN "此脚本暂不支持您的系统: $ID"
+        WARN "此脚本目前不支持您的系统: $ID"
         exit 1
         ;;
 esac
 
-INFO "系统发行版: $NAME"
-INFO "系统版本: $VERSION"
-INFO "系统ID: $ID"
-INFO "系统ID Like: $ID_LIKE"
+INFO "System release:: $NAME"
+INFO "System version: $VERSION"
+INFO "System ID: $ID"
+INFO "System ID Like: $ID_LIKE"
+}
+
+function CHECK_PACKAGE_MANAGER() {
+    if command -v dnf &> /dev/null; then
+        package_manager="dnf"
+    elif command -v yum &> /dev/null; then
+        package_manager="yum"
+    elif command -v apt-get &> /dev/null; then
+        package_manager="apt-get"
+    elif command -v apt &> /dev/null; then
+        package_manager="apt"
+    else
+        ERROR "不受支持的软件包管理器."
+        exit 1
+    fi
+}
+
+function CHECK_PKG_MANAGER() {
+    if command -v rpm &> /dev/null; then
+        pkg_manager="rpm"
+    elif command -v dpkg &> /dev/null; then
+        pkg_manager="dpkg"
+    elif command -v apt &> /dev/null; then
+        pkg_manager="apt"
+    else
+        ERROR "无法确定包管理系统."
+        exit 1
+    fi
+}
+
+function CHECKMEM() {
+memory_usage=$(free | awk '/^Mem:/ {printf "%.2f", $3/$2 * 100}')
+memory_usage=${memory_usage%.*}
+
+if [[ $memory_usage -gt 90 ]]; then
+    read -e -p "$(WARN "内存占用率${LIGHT_RED}高于 70%($memory_usage%)${RESET} 是否继续安装? ${PROMPT_YES_NO}")" continu
+    if [ "$continu" == "n" ] || [ "$continu" == "N" ]; then
+        exit 1
+    fi
+else
+    INFO "内存资源充足.请继续 ${LIGHT_GREEN}($memory_usage%)${RESET}"
+fi
+}
+
+function CHECKFIRE() {
+systemctl stop firewalld &> /dev/null
+systemctl disable firewalld &> /dev/null
+systemctl stop iptables &> /dev/null
+systemctl disable iptables &> /dev/null
+ufw disable &> /dev/null
+INFO "防火墙已被禁用."
+
+if [[ "$repo_type" == "centos" || "$repo_type" == "rhel" ]]; then
+    if sestatus | grep "SELinux status" | grep -q "enabled"; then
+        WARN "SELinux 已启用。禁用 SELinux..."
+        setenforce 0
+        sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+        INFO "SELinux 已被禁用."
+    else
+        INFO "SELinux 已被禁用."
+    fi
+fi
+}
+
+
+function INSTALL_PACKAGE(){
+SEPARATOR "安装依赖"
+INFO "检查依赖安装情况，请稍等 ..."
+TIMEOUT=300
+PACKAGES_APT=(
+    lsof jq wget apache2-utils tar
+)
+PACKAGES_YUM=(
+    epel-release lsof jq wget yum-utils httpd-tools tar
+)
+
+if [ "$package_manager" = "dnf" ] || [ "$package_manager" = "yum" ]; then
+    for package in "${PACKAGES_YUM[@]}"; do
+        if $pkg_manager -q "$package" &>/dev/null; then
+            INFO "${LIGHT_GREEN}已经安装${RESET} $package ..."
+        else
+            INFO "${LIGHT_CYAN}正在安装${RESET} $package ..."
+
+            start_time=$(date +%s)
+
+            $package_manager -y install "$package" --skip-broken > /dev/null 2>&1 &
+            install_pid=$!
+
+            while [[ $(($(date +%s) - $start_time)) -lt $TIMEOUT ]] && kill -0 $install_pid &>/dev/null; do
+                sleep 1
+            done
+
+            if kill -0 $install_pid &>/dev/null; then
+                WARN "$package 的安装时间超过 ${LIGHT_YELLOW}$TIMEOUT 秒${RESET}。是否继续? [${LIGHT_GREEN}y${RESET}/${LIGHT_YELLOW}n${RESET}]"
+                read -r continue_install
+                if [ "$continue_install" != "y" ]; then
+                    ERROR "$package 的安装超时。退出脚本。"
+                    exit 1
+                else
+                    continue
+                fi
+            fi
+
+            wait $install_pid
+            if [ $? -ne 0 ]; then
+                ERROR "$package 安装失败。请检查系统安装源，然后再次运行此脚本！请尝试手动执行安装: ${LIGHT_BLUE}$package_manager -y install $package${RESET}"
+                exit 1
+            fi
+        fi
+    done
+elif [ "$package_manager" = "apt-get" ] || [ "$package_manager" = "apt" ];then
+    dpkg --configure -a &>/dev/null
+    $package_manager update &>/dev/null
+    for package in "${PACKAGES_APT[@]}"; do
+        if $pkg_manager -s "$package" &>/dev/null; then
+            INFO "已经安装 $package ..."
+        else
+            INFO "正在安装 $package ..."
+            $package_manager install -y $package > /dev/null 2>&1
+            if [ $? -ne 0 ]; then
+                ERROR "安装 $package 失败,请检查系统安装源之后再次运行此脚本！请尝试手动执行安装: ${LIGHT_BLUE}$package_manager -y install $package${RESET}"
+                exit 1
+            fi
+        fi
+    done
+else
+    ERROR "无法确定包管理系统,脚本无法继续执行,请检查!"
+    exit 1
+fi
+}
+
+
+function CHECK_DOCKER() {
+status=$(systemctl is-active docker)
+
+if [ "$status" = "active" ]; then
+    INFO "Docker 服务运行正常，请继续..."
+else
+    ERROR "Docker 服务未运行，会导致服务无法正常安装运行，请检查后再次执行脚本！"
+    ERROR "-----------服务启动失败，请查看错误日志 ↓↓↓-----------"
+      journalctl -u docker.service --no-pager
+    ERROR "-----------服务启动失败，请查看错误日志 ↑↑↑-----------"
+    exit 1
+fi
 }
 
 
 function INSTALL_DOCKER() {
-# 定义存储库文件名
+SEPARATOR "安装Docker"
 repo_file="docker-ce.repo"
-# 下载存储库文件
 url="https://download.docker.com/linux/$repo_type"
-
-# 定义最多重试次数
 MAX_ATTEMPTS=3
-# 初始化 attempt和 success变量为0和 false
 attempt=0
 success=false
 
@@ -101,55 +263,55 @@ if [ "$repo_type" = "centos" ] || [ "$repo_type" = "rhel" ]; then
     if ! command -v docker &> /dev/null;then
       while [[ $attempt -lt $MAX_ATTEMPTS ]]; do
         attempt=$((attempt + 1))
-        ERROR "docker 未安装，正在进行安装..."
+        WARN "Docker 未安装，正在进行安装..."
         yum-config-manager --add-repo $url/$repo_file &>/dev/null
-        yum -y install docker-ce &>/dev/null
-        # 检查命令的返回值
+        $package_manager -y install docker-ce &>/dev/null
         if [ $? -eq 0 ]; then
             success=true
             break
         fi
-        ERROR "docker安装失败，正在尝试重新下载 (尝试次数: $attempt)"
+        ERROR "Docker 安装失败，正在尝试重新下载 (尝试次数: $attempt)"
       done
 
       if $success; then
-         INFO "docker 安装版本为：$(docker --version)"
-         systemctl restart docker | grep -E "ERROR|ELIFECYCLE|WARN"
+         INFO "Docker 安装成功，版本为：$(docker --version)"
+         systemctl restart docker &>/dev/null
+         CHECK_DOCKER
          systemctl enable docker &>/dev/null
       else
-         ERROR "docker安装失败，请尝试手动安装"
+         ERROR "Docker 安装失败，请尝试手动安装"
          exit 1
       fi
     else
-      INFO "docker 已安装，安装版本为：$(docker --version)"
+      INFO "Docker 已安装，安装版本为：$(docker --version)"
       systemctl restart docker | grep -E "ERROR|ELIFECYCLE|WARN"
     fi
 elif [ "$repo_type" == "ubuntu" ]; then
     if ! command -v docker &> /dev/null;then
       while [[ $attempt -lt $MAX_ATTEMPTS ]]; do
         attempt=$((attempt + 1))
-        ERROR "docker 未安装，正在进行安装..."
+        WARN "Docker 未安装，正在进行安装..."
         curl -fsSL $url/gpg | sudo apt-key add - &>/dev/null
         add-apt-repository "deb [arch=amd64] $url $(lsb_release -cs) stable" <<< $'\n' &>/dev/null
-        apt-get -y install docker-ce docker-ce-cli containerd.io &>/dev/null
-        # 检查命令的返回值
+        $package_manager -y install docker-ce docker-ce-cli containerd.io &>/dev/null
         if [ $? -eq 0 ]; then
             success=true
             break
         fi
-        ERROR "docker安装失败，正在尝试重新下载 (尝试次数: $attempt)"
+        ERROR "Docker 安装失败，正在尝试重新下载 (尝试次数: $attempt)"
       done
 
       if $success; then
-         INFO "docker 安装版本为：$(docker --version)"
-         systemctl restart docker | grep -E "ERROR|ELIFECYCLE|WARN"
+         INFO "Docker 安装成功，版本为：$(docker --version)"
+         systemctl restart docker &>/dev/null
+         CHECK_DOCKER
          systemctl enable docker &>/dev/null
       else
-         ERROR "docker安装失败，请尝试手动安装"
+         ERROR "Docker 安装失败，请尝试手动安装"
          exit 1
       fi
     else
-      INFO "docker 已安装，安装版本为：$(docker --version)"
+      INFO "Docker 已安装，安装版本为：$(docker --version)"
       systemctl restart docker | grep -E "ERROR|ELIFECYCLE|WARN"
     fi
 elif [ "$repo_type" == "debian" ]; then
@@ -157,32 +319,33 @@ elif [ "$repo_type" == "debian" ]; then
       while [[ $attempt -lt $MAX_ATTEMPTS ]]; do
         attempt=$((attempt + 1))
 
-        ERROR "docker 未安装，正在进行安装..."
+        WARN "Docker 未安装，正在进行安装..."
         curl -fsSL $url/gpg | sudo apt-key add - &>/dev/null
         add-apt-repository "deb [arch=amd64] $url $(lsb_release -cs) stable" <<< $'\n' &>/dev/null
-        apt-get -y install docker-ce docker-ce-cli containerd.io &>/dev/null
-        # 检查命令的返回值
+        $package_manager -y install docker-ce docker-ce-cli containerd.io &>/dev/null
         if [ $? -eq 0 ]; then
             success=true
             break
         fi
-        ERROR "docker安装失败，正在尝试重新下载 (尝试次数: $attempt)"
+        ERROR "Docker 安装失败，正在尝试重新下载 (尝试次数: $attempt)"
       done
 
       if $success; then
-         INFO "docker 安装版本为：$(docker --version)"
-         systemctl restart docker | grep -E "ERROR|ELIFECYCLE|WARN"
+         INFO "Docker 安装成功，版本为：$(docker --version)"
+         systemctl restart docker &>/dev/null
+         CHECK_DOCKER
          systemctl enable docker &>/dev/null
       else
-         ERROR "docker安装失败，请尝试手动安装"
+         ERROR "Docker 安装失败，请尝试手动安装"
          exit 1
       fi
     else
-      INFO "docker 已安装，安装版本为：$(docker --version)"
-      systemctl restart docker | grep -E "ERROR|ELIFECYCLE|WARN"
+        INFO "Docker 已安装，安装版本为：$(docker --version)"
+        systemctl restart docker &>/dev/null
+        CHECK_DOCKER
     fi
 else
-    ERROR "Unsupported operating system."
+    ERROR "不支持的操作系统."
     exit 1
 fi
 }
@@ -211,87 +374,84 @@ INFO
 INFO "================================================================"
 }
 
-function ADD_UPTIME_KUMA() {
-    INFO "=======================开始安装 ======================="
-    read -e -p "$(INFO '是否部署或卸载 uptime-kuma监控工具？(部署请输入 y，卸载请输入 n): ')" uptime
+function INSTALL_SERVER() {
+SEPARATOR "开始安装"
+wget -O $save_path/docker-compose.yaml "$url" &>/dev/null
+#启动 Docker容器
+docker compose up -d
 
-    if [[ "$uptime" == "y" ]]; then
-        #检查是否已经运行了 uptime-kuma容器
-        if docker ps -a --format "{{.Names}}" | grep -q "uptime-kuma"; then
-            WARN "已经运行了uptime-kuma监控工具。"
-            read -e -p "$(WARN '是否停止和删除旧的容器并继续安装？(y/n): ')" continue_install
 
-            if [[ "$continue_install" == "y" ]]; then
-                docker stop uptime-kuma
-                docker rm uptime-kuma
-                WARN "已停止和删除旧的uptime-kuma容器。"
-            else
-                WARN "已取消部署uptime-kuma监控工具。"
-                exit 0
-            fi
-        fi
+#检查 uptime-kuma容器状态
+status_uptime=`docker container inspect -f '{{.State.Running}}' uptime-kuma 2>/dev/null`
 
-        MAX_TRIES=3
-
-        for ((try=1; try<=${MAX_TRIES}; try++)); do
-            read -e -p "$(INFO '请输入监听的端口: ')" UPTIME_PORT
-
-            #检查端口是否已被占用
-            if ss -tulwn | grep -q ":${UPTIME_PORT} "; then
-                ERROR "端口 ${UPTIME_PORT}已被占用，请尝试其他端口。"
-                if [ "${try}" -lt "${MAX_TRIES}" ]; then
-                    WARN "您还有 $((${MAX_TRIES} - ${try}))次尝试机会。"
-                else
-                    ERROR "您已用尽所有尝试机会。"
-                    exit 1
-                fi
-            else
-                break
-            fi
-        done
-
-        #提示用户输入映射的目录
-        read -e -p "$(INFO '请输入数据持久化在宿主机上的目录路径: ')" MAPPING_DIR
-        #检查目录是否存在，如果不存在则创建
-        if [ ! -d "${MAPPING_DIR}" ]; then
-            mkdir -p "${MAPPING_DIR}"
-            INFO "目录已创建：${MAPPING_DIR}"
-        fi
-
-        #启动 Docker容器
-        docker run -d --restart=always -p "${UPTIME_PORT}":3001 -v "${MAPPING_DIR}":/app/data --name uptime-kuma louislam/uptime-kuma:1
-        #检查 uptime-kuma容器状态
-        status_uptime=`docker container inspect -f '{{.State.Running}}' uptime-kuma 2>/dev/null`
-
-        #判断容器状态并打印提示
-        if [[ "$status_uptime" == "true" ]]; then
-            INFO ">>>>> Docker containers are up and running <<<<<"
-            #调用提示信息函数
-            PROMPT
-        else
-            ERROR ">>>>> The following containers are not up <<<<<"
-            if [[ "$status_uptime" != "true" ]]; then
-                ERROR "uptime-kuma安装过程中出现问题，请检查日志或手动验证容器状态。"
-            fi
-        fi
-    elif [[ "$uptime" == "n" ]]; then
-        #卸载 uptime-kuma
-        if docker ps -a --format "{{.Names}}" | grep -q "uptime-kuma"; then
-            docker stop uptime-kuma
-            docker rm uptime-kuma
-            INFO "uptime-kuma已成功卸载。"
-        else
-            WARN "没有找到 uptime-kuma容器，无需卸载。"
-        fi
-    else
-        ERROR "选项错误！请重新运行脚本并选择正确的选项。"
-        exit 1
+#判断容器状态并打印提示
+if [[ "$status_uptime" == "true" ]]; then
+    INFO ">>>>> Docker containers are up and running <<<<<"
+    #调用提示信息函数
+    PROMPT
+else
+    ERROR ">>>>> The following containers are not up <<<<<"
+    if [[ "$status_uptime" != "true" ]]; then
+        ERROR "uptime-kuma 安装过程中出现问题，请检查日志或手动验证容器状态。"
     fi
+fi
 }
 
-main() {
-  CHECK_OS
-  INSTALL_DOCKER
-  ADD_UPTIME_KUMA
+function UNISTALL_SERVER() {
+#卸载 uptime-kuma
+if docker ps -a --format "{{.Names}}" | grep -q "uptime-kuma"; then
+    docker compose down
+    INFO "uptime-kuma 已成功卸载。"
+else
+    WARN "没有找到 uptime-kuma容器，无需卸载"
+fi
 }
-main
+
+function UPDATE_SERVER() {
+#卸载 uptime-kuma
+if docker ps -a --format "{{.Names}}" | grep -q "uptime-kuma"; then
+    docker compose pull
+    docker compose up -d --force-recreate
+    INFO "uptime-kuma已成功升级。"
+else
+    WARN "没有找到 uptime-kuma容器，无法执行升级操作"
+fi
+}
+
+
+function main_menu() {
+SEPARATOR "请选择操作"
+echo -e "1) ${BOLD}${LIGHT_GREEN}安装${RESET}服务"
+echo -e "2) ${BOLD}${LIGHT_MAGENTA}卸载${RESET}服务"
+echo -e "3) ${BOLD}${LIGHT_YELLOW}更新${RESET}服务"
+echo -e "0) ${BOLD}退出脚本${RESET}"
+echo "---------------------------------------------------------------"
+read -e -p "$(INFO "输入${LIGHT_CYAN}对应数字${RESET}并按${LIGHT_GREEN}Enter${RESET}键 > ")" main_choice
+
+
+case $main_choice in
+    1)
+        CHECK_OS
+        CHECK_PACKAGE_MANAGER
+        CHECK_PKG_MANAGER
+        CHECKMEM
+        INSTALL_PACKAGE
+        INSTALL_DOCKER
+        INSTALL_SERVER
+        ;;
+    2)
+        UNISTALL_SERVER
+        ;;
+    3)
+        UPDATE_SERVER
+        ;;
+    0)
+        exit 1
+        ;;
+    *)
+        WARN "输入了无效的选择。请重新${LIGHT_GREEN}选择0-3${RESET}的选项."
+        sleep 2; main_menu
+        ;;
+esac
+}
+main_menu
